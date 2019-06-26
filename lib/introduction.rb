1ebf4e7acf3653373bf890aba5266f0da3520621
jose luis <cortezluisjose@gmail.com>
2019-06-26 05:37:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
   def
-  introdution_with_language_optional(name)
+  introdution_with_language(name)
   "Hi my name is #{name}."
   puts "iam learning to program in #{language}."
-  
+  def 
